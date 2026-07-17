0c76a3b5c5115fb3922a6bbd72baf0cb819f809a
root <root@ip-10-0-15-107.ec2.internal>
2026-07-17 11:12:58 +0000

    new changes

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ def home():
 
 <h2>Application deployed automatically using Jenkins.</h2>
 <h3> Very basic level of CICD with docker+Jenkins!</h3>
+<h3> added github webhook as jenkins website-current-ec2, whenever changes are done in git jenkins will start and the finished images are sent to dockerhub via docker credentials added in jenkins credentials" </h3>
+<h4> vanakkam da mapla</h4>
 
 """
 
