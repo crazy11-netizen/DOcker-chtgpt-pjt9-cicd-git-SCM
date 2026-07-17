0621a94b016ff449d45ae54495b2f74fcfc5828a
root <root@ip-10-0-15-107.ec2.internal>
2026-07-17 11:07:28 +0000

    Testing automation haha

---
+++ jenkins_home/workspace/Docker-cicd-pjt9/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+
+    return """
+
+<h1>CI/CD Pipeline Working!</h1>
+
+<h2>Application deployed automatically using Jenkins.</h2>
+<h3> Very basic level of CICD with docker+Jenkins!</h3>
+<h3> added github webhook as jenkins website-current-ec2, whenever changes are done in git jenkins will start and the finished images are sent to dockerhub via docker credentials added in jenkins credentials" </h3>
+<h4> vanakkam da mapla</h4>
+"""
+
+app.run(host="0.0.0.0",port=5000)
